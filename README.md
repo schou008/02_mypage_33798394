8cd06687e7af91f4dacd73599131544c0fc385cd
schou008​ <saqibchy9@gmail.com>
2025-10-08 09:58:03 +0100

    Added Comments to index.js file and Added README.md file

---
+++ README.md
@@ -0,0 +1,10 @@
+# MyPage
+A web application using node.js talking about me
+
+Version 1.0
+-Added index.js file
+-Added package.json file
+
+Version 1.1
+-Added README.md file
+-Added comments to index.js file for ease of reading
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+//My Page Web App using node.js
+
 var http = require("http");
+//Port Number
 const port = 8000;
 
 http.createServer(function(req, res) {
@@ -8,11 +11,15 @@ http.createServer(function(req, res) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //Title of Web Application
     <title>My Page Web App</title>
 </head>
 <body>
+    //Header 1 of Web Application
     <h1>Welcome to My Page Web App</h1>
+    //Header 2 of Web Application
     <h2>About Me</h2>
+    //Text explaining About Me
     <p>My name is Saqib Choudhury</p>
 </body>
 </html>`);

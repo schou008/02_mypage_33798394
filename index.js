88181cf9c699a81d2b226fd7157a677b4403a1c2
schou008​ <saqibchy9@gmail.com>
2025-10-15 09:52:51 +0100

    Added Comments and Code to index.js file to make it easier to understand

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ http.createServer(function(req, res) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Title of Web Application -->
-    <title>My Page Web App</title>
+    <title>My Page Web Application</title>
 </head>
 <body>
-    <!-- Header 1 of Web Application -->
-    <h1>Welcome to My Page Web App</h1>
-    <!-- Header 2 of Web Application -->
+    <!-- Header 1 - Title of Web Application -->
+    <h1>Welcome to My Page Web Application</h1>
+    <!-- Header 2 - About Me of Web Application -->
     <h2>About Me</h2>
     <!-- Text explaining About Me -->
     <p>My name is Saqib Choudhury</p>
 </body>
 </html>`);
 }).listen(port, function() {
+    // Running the local Port Number
     console.log(`Node server is running on port ${port}...`);
 });
